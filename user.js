2c4535e4a14cc3b599887f4c2a9df2267eaf23b7
Suzana Jovanovic <code.s09@outlook.com>
2021-02-23 13:12:09 +0100

    end

---
+++ user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose;
+
+
+const userSchema = new Schema (
+     {
+ vorname: String,
+ nachname: String,
+ email: String,
+ password: String
+
+}
+);
+
+module.exports = mongoose.model("User", userSchema)
